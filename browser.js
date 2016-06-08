388f9a4a23bfa4a1b4897b384312d3234ac67b17
Adam Benzan <adam.benzan@gmail.com>
2016-06-08 13:08:47 -0400

    play with workers

---
+++ browser.js
@@ -0,0 +1,55 @@
+var jscw = require("./src/crossword");
+var nytwords = require('./src/nytwords');
+
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function makeSquare(size, wordlist) {
+  var cells = [], across = {}, down = {};
+  
+  for (var i = 0; i < size; i++) {
+    across[i * size] = {
+      length: size
+    }
+    down[i] = {
+      length: size
+    }
+  }
+
+  for (var j = 0; j < size * size; j++) {
+    cells.push("_");
+  }
+
+  return new jscw.Crossword(cells, across, down,
+                            jscw.WordList(
+                              shuffle(
+                                nytwords.filter(function(e) {
+                                  return e.length == size;
+                                })
+                              )
+                            ));
+}
+
+self.addEventListener("message", function(message) {
+  jscw.solve(makeSquare(message.data.size), function() {}, function (result) {
+    self.postMessage(result.toString());
+  });  
+})
+  
